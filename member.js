0ae904970f791e069576f5678cc4be7790f543e1
Shubham Kumar <shubhamkr.997372@gmail.com>
2026-05-15 12:50:55 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember(){
+    return {
+        name: 'skills-member',
+        template: `
+            <div class="skills-member">
+                <div class="skills-member__avatar">
+                    <img :src="member.avatar" alt="Avatar">
+                </div>
+                <div class="skills-member__info">
+                    <h3>{{ member.name }}</h3>
+                    <p>{{ member.role }}</p>
+                </div>
+            </div>
+        `,
+        props: {
+            member: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+}
+
+export default skillsMember;    
